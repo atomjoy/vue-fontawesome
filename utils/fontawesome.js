8e3f9ddff8ad0be5b5f3e1b065729926283bcd79
Atomjoy <atomjoy.official@gmail.com>
2024-03-27 10:04:02 +0100

    Vue3 fontawesome

---
+++ utils/fontawesome.js
@@ -0,0 +1,21 @@
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+library.add(fas, far, fab, faPhone)
+dom.watch();
+
+export default FontAwesomeIcon;
+
+/* Import in app.js */
+// import FontAwesomeIcon from "./utils/fontawesome";
+// app.component("FontAwesomeIcon", FontAwesomeIcon)
+
+/* Install */
+// npm i --save @fortawesome/vue-fontawesome@latest-3
+// npm i --save @fortawesome/fontawesome-svg-core
+// npm i --save @fortawesome/free-solid-svg-icons
+// npm i --save @fortawesome/free-regular-svg-icons
+// npm i --save @fortawesome/free-brands-svg-icons
