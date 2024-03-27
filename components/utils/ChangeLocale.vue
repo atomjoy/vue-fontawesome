8f46cc264f8c857e9e24a6a1bf5ce743d70939d4
Atomjoy <atomjoy.official@gmail.com>
2024-03-27 10:30:21 +0100

    Update ChangeLocale.vue

--- components/utils/ChangeLocale.vue
+++ components/utils/ChangeLocale.vue
@@ -10,6 +10,7 @@
 import { watch, computed, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
 // import { useAuthStore } from '@/views/page/auth/stores/auth.js'
+
 // const store = useAuthStore()
 const { t, locale, availableLocales } = useI18n({ useScope: 'global' })
 
